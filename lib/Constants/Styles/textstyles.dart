88a1b0a805f4691da6f369c71e04a4b3fa1a80a8
Vibhor Vats <vibhordev7@gmail.com>
2022-09-27 18:57:30 +0530

    Fields Displayed

---
+++ lib/Constants/Styles/textstyles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: Colors.black87,
+);
+
+const kSubtitleTextStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  color: Colors.black87,
+);
--- lib/Screens/Home/View/home_screen.dart
+++ lib/Screens/Home/View/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:profile_edit_task/Constants/Styles/textstyles.dart';
 import 'package:profile_edit_task/Routes/routes.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -36,6 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
             borderRadius: BorderRadius.circular(20),
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -49,8 +51,9 @@ class _HomeScreenState extends State<HomeScreen> {
                       padding: const EdgeInsets.symmetric(
                           horizontal: 10, vertical: 5),
                       decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.6),
-                          borderRadius: BorderRadius.circular(20)),
+                        color: Colors.white.withOpacity(0.6),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                       child: const Text(
                         'Update Profile',
                         style: TextStyle(fontSize: 10, color: Colors.black87),
@@ -59,6 +62,86 @@ class _HomeScreenState extends State<HomeScreen> {
                   )
                 ],
               ),
+
+              //Fields
+              Container(
+                width: mediaQuery.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white.withOpacity(0.7)),
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Name',
+                      style: kTitleTextStyle,
+                    ),
+                    Text(
+                      'Vibhor Vats',
+                      style: kSubtitleTextStyle,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Email',
+                      style: kTitleTextStyle,
+                    ),
+                    Text(
+                      'vibhor.stav@gmail.com',
+                      style: kSubtitleTextStyle,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'City',
+                      style: kTitleTextStyle,
+                    ),
+                    Text(
+                      'Guragon',
+                      style: kSubtitleTextStyle,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'State',
+                      style: kTitleTextStyle,
+                    ),
+                    Text(
+                      'Haryana',
+                      style: kSubtitleTextStyle,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Country',
+                      style: kTitleTextStyle,
+                    ),
+                    Text(
+                      'India',
+                      style: kSubtitleTextStyle,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Phone',
+                      style: kTitleTextStyle,
+                    ),
+                    Text(
+                      '+91 9898989898',
+                      style: kSubtitleTextStyle,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
